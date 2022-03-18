ad45ebb87c8c69f619673c99c6f7c5c82fb3dc19
Nayhan Provedo Labrada <nayhanprovedolabrada@gmail.com>
2022-03-18 15:31:18 -0400

    comienzo del proyecto

---
+++ BEAssistant/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BEAssistant
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonEstadisticas_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonElementos_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonOperaciones_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonInverciones_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonPropuestas_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonRespuestasR_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
