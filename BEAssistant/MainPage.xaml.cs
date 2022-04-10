aa6589f502191700288a1f887b0283bc9dccb3c6
Nayhan Provedo Labrada <nayhanprovedolabrada@gmail.com>
2022-04-10 03:58:57 -0400

    created and implemented everything related to daily check-ins

--- BEAssistant/MainPage.xaml.cs
+++ BEAssistant/MainPage.xaml.cs
@@ -18,21 +18,82 @@ namespace BEAssistant
         }
 
         private async void Iniciar()
-        {
-            bool yaCierreDiario = false;
-            var eventos = await App.Database.GetEventos();
-            //await App.Database.DeleteEventos(eventos[0]);
-            foreach (var item in eventos)
-            { if (item.Evento == "CierreDiario") yaCierreDiario = true; }
-            if(!yaCierreDiario)
+        {           
+            DateTime TimeActual = DateTime.Now;
+            TimeActual = TimeActual.ToLocalTime();
+            var FechaInicio = await App.Database.GetIdFechaInicio(1);
+            if(FechaInicio == null)
             {
-                await App.Database.SaveEventos(new Eventos()
+                await App.Database.SaveFechaInicio(new FechaInicio
                 {
-                    Evento = "CierreDiario"
+                    inicio = TimeActual
                 });
-                DependencyService.Get<IDemoServices>().Start();
             }
+            else
+            {
+                if(FechaInicio.inicio.DayOfYear != TimeActual.DayOfYear || FechaInicio.inicio.Year != TimeActual.Year)
+                {
+                    DateTime timeRevisar = new DateTime();
+                    bool actualizar = false;
+                    DateTime limiteDeRevicion = new DateTime();
+                    var lastDate = await App.Database.GetLastItemCierreDiario();
+                    if (lastDate.Count == 0) 
+                    { 
+                        actualizar = true;
+                        timeRevisar = FechaInicio.inicio;
+                    }
+                    if (lastDate.Count > 0)
+                    {
+                        if (lastDate[0].Fecha.DayOfYear < TimeActual.DayOfYear - 1 && lastDate[0].Fecha.Year == TimeActual.Year) actualizar = true;
+                        if (lastDate[0].Fecha.DayOfYear < TimeActual.DayOfYear - 1 + 365 && lastDate[0].Fecha.Year < TimeActual.Year) actualizar = true;
+                        timeRevisar = lastDate[0].Fecha;
+                        timeRevisar.AddDays(1);
+                    }
+                    limiteDeRevicion = TimeActual;
+                    if (actualizar)
+                    {
+                        
 
+                        var cons = await App.Database.GetRegConstante();
+                        var acum = await App.Database.GetRegAcumulativa();
+                        var extra = await App.Database.GetRegExtraordinaria();
+                        var venta = await App.Database.GetVenta();
+                        while (timeRevisar.Year != limiteDeRevicion.Year || timeRevisar.Month != limiteDeRevicion.Month || timeRevisar.Day != limiteDeRevicion.Day)
+                        {
+                            double gasto = 0;
+                            double ganacia = 0;
+
+                            
+                            if (cons.Count > 0) 
+                            { var elems = cons.FindAll(x => x.Fecha.DayOfYear == timeRevisar.DayOfYear && x.Fecha.Year == timeRevisar.Year);
+                                foreach (var item in elems)  { gasto += (item.Costo * item.Unidades); } }
+                            
+                            if (acum.Count > 0)
+                            { var elems = acum.FindAll(x => x.Fecha.DayOfYear == timeRevisar.DayOfYear && x.Fecha.Year == timeRevisar.Year);
+                                foreach (var item in elems) { gasto += (item.Costo * item.Unidades); } }
+                            
+                            if (extra.Count > 0)
+                            { var elems = extra.FindAll(x => x.Fecha.DayOfYear == timeRevisar.DayOfYear && x.Fecha.Year == timeRevisar.Year);
+                                foreach (var item in elems) { gasto += (item.Costo * item.Unidades); } }
+
+                            
+                            if (venta.Count > 0) 
+                            { var elems = venta.FindAll(x => x.Fecha.DayOfYear == timeRevisar.DayOfYear && x.Fecha.Year == timeRevisar.Year);
+                                foreach (var item in elems) { ganacia += (item.Precio * item.Unidades); } }
+
+                            await App.Database.SaveCierreDiario(new CierresDiario()
+                            {
+                                Fecha = new DateTime(year: timeRevisar.Year, month: timeRevisar.Month, day: timeRevisar.Day),
+                                Ganancia = ganacia,
+                                Gasto = gasto
+                            });
+
+                            timeRevisar = timeRevisar.AddDays(1);
+                        }
+
+                    }
+                }
+            }                     
         }
 
         private async void ButtonEstadisticas_Clicked(object sender, EventArgs e)
