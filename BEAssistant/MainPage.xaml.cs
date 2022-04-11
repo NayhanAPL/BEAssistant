9f5b1db9385f6d331ecef140e7b5b857025f437d
Nayhan Provedo Labrada <nayhanprovedolabrada@gmail.com>
2022-04-11 03:58:53 -0400

    daily and monthly closing done. regConstante update

--- BEAssistant/MainPage.xaml.cs
+++ BEAssistant/MainPage.xaml.cs
@@ -14,15 +14,19 @@ namespace BEAssistant
         public MainPage()
         {
             InitializeComponent();
+            
             Iniciar();
         }
-
         private async void Iniciar()
-        {           
+        {
+            RegistroConstante();
+            var d = await App.Database.GetCierreDiario();
+            var m = await App.Database.GetCierreMensual();
+
             DateTime TimeActual = DateTime.Now;
             TimeActual = TimeActual.ToLocalTime();
             var FechaInicio = await App.Database.GetIdFechaInicio(1);
-            if(FechaInicio == null)
+            if (FechaInicio == null)
             {
                 await App.Database.SaveFechaInicio(new FechaInicio
                 {
@@ -31,14 +35,27 @@ namespace BEAssistant
             }
             else
             {
-                if(FechaInicio.inicio.DayOfYear != TimeActual.DayOfYear || FechaInicio.inicio.Year != TimeActual.Year)
+                if (FechaInicio.inicio.DayOfYear != TimeActual.DayOfYear || FechaInicio.inicio.Year != TimeActual.Year)
                 {
                     DateTime timeRevisar = new DateTime();
                     bool actualizar = false;
                     DateTime limiteDeRevicion = new DateTime();
                     var lastDate = await App.Database.GetLastItemCierreDiario();
-                    if (lastDate.Count == 0) 
-                    { 
+                    var lastRegMes = await App.Database.GetLastItemCierreMensual();
+                    if (lastRegMes.Count == 0)
+                    {
+                        await App.Database.SaveCierreMensual(new CierresMensual()
+                        {
+                            GastoE = 0,
+                            GastoA = 0,
+                            GastoC = 0,
+                            Ganancia = 0,
+                            Fecha = FechaInicio.inicio
+                        });
+                        lastRegMes = await App.Database.GetLastItemCierreMensual();
+                    }
+                    if (lastDate.Count == 0)
+                    {
                         actualizar = true;
                         timeRevisar = FechaInicio.inicio;
                     }
@@ -52,7 +69,7 @@ namespace BEAssistant
                     limiteDeRevicion = TimeActual;
                     if (actualizar)
                     {
-                        
+
 
                         var cons = await App.Database.GetRegConstante();
                         var acum = await App.Database.GetRegAcumulativa();
@@ -60,44 +77,135 @@ namespace BEAssistant
                         var venta = await App.Database.GetVenta();
                         while (timeRevisar.Year != limiteDeRevicion.Year || timeRevisar.Month != limiteDeRevicion.Month || timeRevisar.Day != limiteDeRevicion.Day)
                         {
-                            double gasto = 0;
+                            double gastoC = 0;
+                            double gastoA = 0;
+                            double gastoE = 0;
                             double ganacia = 0;
 
-                            
-                            if (cons.Count > 0) 
+
+                            if (cons.Count > 0)
                             { var elems = cons.FindAll(x => x.Fecha.DayOfYear == timeRevisar.DayOfYear && x.Fecha.Year == timeRevisar.Year);
-                                foreach (var item in elems)  { gasto += (item.Costo * item.Unidades); } }
-                            
+                                foreach (var item in elems) { gastoC += (item.Costo * item.Unidades); } }
+
                             if (acum.Count > 0)
                             { var elems = acum.FindAll(x => x.Fecha.DayOfYear == timeRevisar.DayOfYear && x.Fecha.Year == timeRevisar.Year);
-                                foreach (var item in elems) { gasto += (item.Costo * item.Unidades); } }
-                            
+                                foreach (var item in elems) { gastoA += (item.Costo * item.Unidades); } }
+
                             if (extra.Count > 0)
                             { var elems = extra.FindAll(x => x.Fecha.DayOfYear == timeRevisar.DayOfYear && x.Fecha.Year == timeRevisar.Year);
-                                foreach (var item in elems) { gasto += (item.Costo * item.Unidades); } }
+                                foreach (var item in elems) { gastoE += (item.Costo * item.Unidades); } }
 
-                            
-                            if (venta.Count > 0) 
+
+                            if (venta.Count > 0)
                             { var elems = venta.FindAll(x => x.Fecha.DayOfYear == timeRevisar.DayOfYear && x.Fecha.Year == timeRevisar.Year);
                                 foreach (var item in elems) { ganacia += (item.Precio * item.Unidades); } }
 
-                            await App.Database.SaveCierreDiario(new CierresDiario()
+                            await App.Database.SaveCierreDiario(new CierresDiarios()
                             {
                                 Fecha = new DateTime(year: timeRevisar.Year, month: timeRevisar.Month, day: timeRevisar.Day),
                                 Ganancia = ganacia,
-                                Gasto = gasto
+                                GastoC = gastoC,
+                                GastoA = gastoA,
+                                GastoE = gastoE
                             });
+
+                            lastRegMes = await App.Database.GetLastItemCierreMensual();
+                            if (lastRegMes[0].Fecha.Month != timeRevisar.Month) 
+                            {
+                                await App.Database.SaveCierreMensual( new CierresMensual()
+                                {
+                                    GastoE = gastoE,
+                                    GastoA = gastoA,
+                                    GastoC = gastoC,
+                                    Ganancia = ganacia,
+                                    Fecha = new DateTime(day:1, year: timeRevisar.Year, month: timeRevisar.Month)
+                                }); 
+                            }
+                            else 
+                            {
+                                CierresMensual mensual = new CierresMensual()
+                                {
+                                    Id = lastRegMes[0].Id,
+                                    GastoE = lastRegMes[0].GastoE + gastoE,
+                                    GastoA = lastRegMes[0].GastoA + gastoA,
+                                    GastoC = lastRegMes[0].GastoC + gastoC,
+                                    Ganancia = lastRegMes[0].Ganancia + ganacia,
+                                    Fecha = lastRegMes[0].Fecha
+                                }; await App.Database.SaveUpCierreMensual(mensual);
+                            }
 
                             timeRevisar = timeRevisar.AddDays(1);
                         }
 
                     }
                 }
-            }                     
+
+                
+            }            
+        }
+
+        private async void RegistroConstante()
+        {
+            DateTime TimeActual = DateTime.Now;
+            TimeActual = TimeActual.ToLocalTime();
+            var listaConstante = await App.Database.GetInvConstante();
+            if(listaConstante.Count > 0)
+            {
+                foreach (var elemC in listaConstante)
+                {
+                    DateTime ultimoReg = new DateTime();
+                    var listaReg = await App.Database.GetIdInvRegConstante(elemC.Id);
+                    if (listaReg.Count == 0) ultimoReg = elemC.Fecha;
+                    if (listaReg.Count > 0) ultimoReg = listaReg[listaReg.Count - 1].Fecha;
+
+                    ultimoReg = IncFrecuencia(elemC.Frecuencia, ultimoReg);
+                    
+                    while (ultimoReg.DayOfYear < TimeActual.DayOfYear - 1 && ultimoReg.Year == TimeActual.Year 
+                        || ultimoReg.DayOfYear < TimeActual.DayOfYear - 1 + 365 && ultimoReg.Year < TimeActual.Year)
+                    {
+                        await App.Database.SaveRegConstante(new ReConstante() { 
+                        IdInv = elemC.Id,
+                        Costo = listaReg[listaReg.Count - 1].Costo,
+                        Unidades = listaReg[listaReg.Count - 1].Unidades,
+                        Fecha = new DateTime(year: ultimoReg.Year, month: ultimoReg.Month, day: ultimoReg.Day)
+                        });
+                        IncFrecuencia(elemC.Frecuencia, ultimoReg);
+                    }
+                }
+            }
+              
+        }
+
+        private DateTime IncFrecuencia(ConstFrecuencia frecuencia, DateTime ultimoReg)
+        {
+            switch (frecuencia)
+            {
+                case ConstFrecuencia.Diaria:
+                    ultimoReg = ultimoReg.AddDays(1);
+                    break;
+                case ConstFrecuencia.Semanal:
+                    ultimoReg = ultimoReg.AddDays(7);
+                    break;
+                case ConstFrecuencia.Quincenal:
+                    ultimoReg = ultimoReg.AddDays(15);
+                    break;
+                case ConstFrecuencia.Mensual:
+                    ultimoReg = ultimoReg.AddMonths(1);
+                    break;
+                case ConstFrecuencia.Semestral:
+                    ultimoReg = ultimoReg.AddMonths(6);
+                    break;
+                case ConstFrecuencia.Anual:
+                    ultimoReg = ultimoReg.AddYears(1);
+                    break;
+                default:
+                    break;
+            }
+            return ultimoReg;
         }
 
         private async void ButtonEstadisticas_Clicked(object sender, EventArgs e)
-        {           
+        {
             await Navigation.PushModalAsync(new TabbedEstadisticas());
         }
 
